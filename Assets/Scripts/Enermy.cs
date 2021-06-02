c5915261242656d3445abcf7787317dcb0b1aac0
persona2740 <80808162+persona2740@users.noreply.github.com>
2021-06-02 15:11:20 +0900

    umjoonsick

---
+++ Assets/Scripts/Enermy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rig;
+    private bool scored = false;
+    public float speed = 20f;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) < 10f)
+        {
+            Vector3 trans = target.position - transform.position;
+            rig.AddForce(trans * speed * Time.deltaTime);
+        }
+ 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && scored == false)
+        {
+            scored = true;
+            GameManager.instance.AddScore(1);
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
+            scored = false;
+        }
+    }
+}
